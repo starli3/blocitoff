fddb717177a4d997d39897ed659bb196ace0c113
Star Li <star.estella.li@gmail.com>
2014-03-23 13:55:44 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blocitoff::Application.config.secret_token = 'a88d0e39af31d89acca7d0b0d1dd7c120f0cbf18b41c5255f9b89af04f5f07663728ba0c7c341c42ccefd882df1a2922991a7cf31a8df911de430c48eb56450c'
